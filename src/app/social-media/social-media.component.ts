8c3091d727fd4c0491f033e895464553d8b99cb8
Vinícius Matiola Tramontin <viniciusmatiolatramontin1@gmail.com>
2025-07-06 11:55:07 -0300

    Dark mode, header e aba de redes sociais implementados

---
+++ src/app/social-media/social-media.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { MatListModule } from '@angular/material/list';
+
+@Component({
+  selector: 'social-media',
+  imports: [MatListModule],
+  templateUrl: './social-media.component.html',
+  styleUrl: './social-media.component.css'
+})
+export class SocialMediaComponent {
+
+}
