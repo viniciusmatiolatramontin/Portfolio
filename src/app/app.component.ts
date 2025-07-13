cbadfe5cbfe102800af4056746e06a2376b542a8
Vinícius Matiola Tramontin <viniciusmatiolatramontin1@gmail.com>
2025-07-13 15:58:11 -0300

    Início da implementação do FAQ

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { MainPageComponent } from './main-page/main-page.component';
+import { MainPageComponent } from './components/main-page/main-page.component';
 
 @Component({
   selector: 'app-root',
--- src/app/components/main-page/main-page.component.ts
+++ src/app/components/main-page/main-page.component.ts
@@ -2,13 +2,15 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { ProjectListComponent } from "../project-list/project-list.component";
 import { MatCardModule } from '@angular/material/card';
 import { MatTabsModule } from '@angular/material/tabs';
+import { MatTreeModule } from '@angular/material/tree';
+import { FaqComponent } from '../faq/faq.component';
 import { SocialMediaComponent } from "../social-media/social-media.component";
 
 @Component({
   selector: 'app-main-page',
   templateUrl: './main-page.component.html',
   styleUrl: './main-page.component.css',
-  imports: [ProjectListComponent, MatCardModule, MatTabsModule, SocialMediaComponent],
+  imports: [ProjectListComponent, MatCardModule, MatTabsModule, SocialMediaComponent, MatTreeModule, FaqComponent],
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class MainPageComponent {
